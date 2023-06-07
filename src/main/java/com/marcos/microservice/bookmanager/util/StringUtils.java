3fb2972a390f0ed3525a7ca5e2e766affb432799
marcos.giovanni <marcos.giovanni@superside.com>
2023-06-07 13:00:18 +0100

    feat: add StringUtils

--- src/main/java/com/marcos/microservice/bookmanager/util/StringUtils.java
+++ src/main/java/com/marcos/microservice/bookmanager/util/StringUtils.java
@@ -1,4 +1,8 @@
 package com.marcos.microservice.bookmanager.util;
 
 public class StringUtils {
+    public static String Sanitise(String input) {
+        String regex = "[^\\p{L}\\p{N}\\p{P}\\p{Z}]";
+        return input.replaceAll(regex, "");
+    }
 }
